215b9e4f898516e6f98fc03b9b74b496d5378683
Ahmed Memon <ahmedpubgking3388@gmail.com>
2025-12-17 21:14:15 +0500

    Delete Auto-Inventory-Project directory

--- Auto-Inventory-Project/data.py
+++
@@ -1,7 +0,0 @@
-import requests
-headers = {"X-API-KEY": "339068c025e4592ac0c7294b7cf830c2b9730c89"}
-payload = {
-        "q": "Search about smartWatches trend in pakistan",
-}
-res = requests.post("https://google.serper.dev/search", json=payload, headers=headers)
-print(res.json().get("organic", []))
